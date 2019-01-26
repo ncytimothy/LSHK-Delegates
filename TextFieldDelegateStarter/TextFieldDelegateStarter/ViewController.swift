6f26802e62313b3a83394f30b6505ed5cea26834
Tim Ng <timothyng2008@me.com>
2019-01-26 16:43:29 +0800

    fix: Fix git conflicts

--- TextFieldDelegateStarter/TextFieldDelegateStarter/ViewController.swift
+++ TextFieldDelegateStarter/TextFieldDelegateStarter/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var switchTextField: UITextField!
     @IBOutlet weak var textFieldSwitch: UISwitch!
     
+    // Initialize the delegates
     
     override func viewDidLoad() {
         super.viewDidLoad()
